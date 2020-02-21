63942e561e9edbe8b5e5b27fe39df11214063696
myke-roly <myke_roly@hotmail.com>
2020-02-21 00:41:32 -0300

    update package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+
 //settings
 app.set('port', 4000);
 app.set('views', path.join(__dirname, 'views'));
@@ -11,11 +12,8 @@ app.set('view engine', 'ejs');
 // routes
 app.use(require('./routes/index'));//importamos un modulo.
 
-
-
 // stattis files
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 //listening server
 app.listen(app.get('port'), (req,res) => {
